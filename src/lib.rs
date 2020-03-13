aaa88f71561b9ce99d45c16f8caac6a1594f792c
Ian Jackson <ijackson@chiark.greenend.org.uk>
2020-03-13 17:20:18 +0000

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,65 @@
-//
+
+use std::env;
+
+use thiserror::Error;
+
+use tokio::net::TcpStream;
+use tokio::prelude::*;
+
+#[derive(Error,Debug)]
+pub enum Error {
+  #[error("env var {0} contains non-unicode data")]
+  EnvNotUnicode(String),
+  #[error("env var {0} value could not be parsed")]
+  EnvInvalidSyntax(String),
+  #[error("socket trouble communicating with pigpiod")]
+  Comms   Socket
+}
+
+type Result<T> = std::result::Result<T,Error>;
+
+pub struct BoardConnection {
+  conn : tokio::net::TcpStream;
+}
+
+static const PI_ENVPORT : &str = "PIGPIO_PORT";
+static const PI_ENVADDR : &str = "PIGPIO_ADDR";
+static const PI_DEFAULT_SOCKET_PORT : u16 = 8888;
+static const PI_DEFAULT_SOCKET_ADDR : &str = "localhost";
+
+
+fn env_var(varname : &str) -> Result<Option<String>> {
+  use std::env::VarError::*;
+  match env::var(varname) {
+    Ok(val) => Ok(val),
+    Err(NotPresent) => Ok(None),
+    Err(NotUnicode(_)) => Err(Error::EnvNotUnicode(varname.to_owned())),
+  }
+}
+
+fn default_port() -> Result<u16> {
+  let spec = env_var(PI_ENVPORT)?;
+  if let None = spec { return Ok(PI_DEFAULT_SOCKET_PORT); }
+  let port : u16 = spec.parse()
+    .or_else(|_| Err(Error::ErrInvalidSyntax(spec.to_owned())))?;
+  Ok(port)
+}
+
+fn default_addr() -> Result<String> {
+  let spec = env_var(PI_ENVADDR);
+  if let None = spec { return Ok(PI_DEFAULT_SOCKET_ADDR.to_owned()); }
+  return spec.to_owned();
+}
+
+impl BoardConnection {
+  pub async fn new_at(addr : &SocketAddr) -> Result<BoardConnection()> {
+    let conn = TcpStream::connect(addr).await;
+    Ok(BoardConnection { conn })
+  }
+
+  pub async fn new() -> Result<BoardConnection()> {
+    let sockaddr = (default_addr()?, default_port()?);
+    let conn = TcpStream::connect(&sockaddr).await;
+    Ok(BoardConnection { conn })
+  }
+}
