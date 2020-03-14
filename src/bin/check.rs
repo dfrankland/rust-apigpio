2557627386c8fbfcfac4c10b845535e14687c51b
Ian Jackson <ijackson@chiark.greenend.org.uk>
2020-03-14 11:26:30 +0000

    conn not mut

--- src/bin/check.rs
+++ src/bin/check.rs
@@ -3,7 +3,7 @@ use apigpio::*;
 
 #[tokio::main]
 async fn main() {
-  let mut conn = Connection::new().await.expect("connect");
+  let conn = Connection::new().await.expect("connect");
   println!("connected");
   let m = conn.get_mode(14).await.expect("getmode");
   println!("gpio mode {}", m);
