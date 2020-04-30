c6d7ce41f47dba8739e3cfa4a0acd4084ed0a163
Ian Jackson <ijackson@chiark.greenend.org.uk>
2020-04-30 14:47:01 +0100

    derive Hash for Level
    Signed-off-by: Ian Jackson <ijackson@chiark.greenend.org.uk>

--- src/lib.rs
+++ src/lib.rs
@@ -103,7 +103,7 @@ pub enum GpioMode {
   Alt5   = PI_ALT5 as isize,
 }
 
-#[derive(Debug,Display,PartialEq,Eq,Copy,Clone,Ord,PartialOrd)]
+#[derive(Debug,Display,PartialEq,Eq,Copy,Clone,Ord,PartialOrd,Hash)]
 pub enum Level {
   L = 0,
   H = 1,
